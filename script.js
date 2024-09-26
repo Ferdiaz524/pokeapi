e8c6af79d24c71af56ae9bc748116fe613be375b
Ferdiaz524 <00351519@uca.edu.sv>
2024-09-26 16:16:29 -0600

    ✨New features: adding new feature

--- script.js
+++ script.js
@@ -12,20 +12,20 @@ document.getElementById('searchBtn').addEventListener('click', function() {
             }
         })
         .then(data => {
-            // Crear un nuevo contenedor para el Pokémon encontrado
+            const alturaEnMetros = data.height / 10;
+            const pesoEnKg = data.weight / 10;   
+
             const pokemonCard = document.createElement('div');
             pokemonCard.classList.add('pokemon-card');
             
-            // Insertar el contenido del Pokémon
             pokemonCard.innerHTML = `
                 <h2>${data.name.toUpperCase()}</h2>
                 <img src="${data.sprites.front_default}" alt="${data.name}">
-                <p><strong>Altura:</strong> ${data.height}</p>
-                <p><strong>Peso:</strong> ${data.weight}</p>
+                <p><strong>Altura:</strong> ${alturaEnMetros} m</p>
+                <p><strong>Peso:</strong> ${pesoEnKg} kg</p>
                 <p><strong>Tipo:</strong> ${data.types.map(typeInfo => typeInfo.type.name).join(', ')}</p>
             `;
 
-            // Insertar la tarjeta del Pokémon al principio del contenedor de resultados
             const pokemonResults = document.getElementById('pokemonResults');
             pokemonResults.insertBefore(pokemonCard, pokemonResults.firstChild);
         })
